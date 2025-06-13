726dc8e261fb88e50d904fcf4a681d490e0e431a
Kene323 <ezemma789@gmail.com>
2025-06-13 13:13:01 -0500

    Tamper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UseReducer from './components/ui/UseReducer'
+import Test from './components/ui/Test'
 
 const App = () => {
   return (
     <div>
       <UseReducer/>
+      <Test/>
     </div>
   )
 }
